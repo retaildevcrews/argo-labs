19f3426380ae3d622287bc676311146a2a40cdd5
Jorge <jorge.luna@utexas.edu>
2023-04-25 19:06:17 +0000

    fixing appending host.k3d.internal to hosts file

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -23,6 +23,6 @@ sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 
 # install flux completions for zsh
-sudo echo '0.0.0.0         host.k3d.internal' >> /etc/hosts
+sudo echo '0.0.0.0         host.k3d.internal' | sudo tee -a /etc/hosts
 
 echo "on-create completed" >> $HOME/status
