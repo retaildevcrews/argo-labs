3c07bbd2334e858950e48295b89f3e1665323c49
Joaquin Rodriguez <rjoaquin@microsoft.com>
2023-07-07 11:46:49 -0500

    Added retry logic to script

--- argo-400k/helper_scripts/create_guestbook.sh
+++ argo-400k/helper_scripts/create_guestbook.sh
@@ -6,6 +6,11 @@ if [ $# -ne 3 ]; then
   exit 1
 fi
 
+if [[ -z "$ARGOCD_LOGIN_SERVER" ]] || [[ -z "$ARGOCD_PASSWORD" ]]; then
+  echo "Error: ARGOCD_LOGIN_SERVER and ARGOCD_PASSWORD must be set."
+  exit 1
+fi
+
 start=$1
 stop=$2
 batch_interval=$3
@@ -15,6 +20,7 @@ echo "start: $start"
 echo "stop: $stop"
 echo "batch_interval: $batch_interval"
 echo "ARGOCD_LOGIN_SERVER: $ARGOCD_LOGIN_SERVER"
+
 argocd login $ARGOCD_LOGIN_SERVER --username admin --password $ARGOCD_PASSWORD --insecure
 
 # Iterate and execute the argocd command
@@ -26,10 +32,30 @@ do
 
   # Execute the argocd command
   echo "Deploying app: $app_name"
-  if ! argocd app create $app_name --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --sync-policy none --sync-option CreateNamespace=true --dest-namespace $app_name --dest-server https://kubernetes.default.svc --directory-recurse --upsert -l testing=argoperf; then
-    echo "Failed to execute argocd command. Stopping execution."
-    exit 1
-  fi
+
+  retry_count=0
+  max_retries=5
+
+  while ((retry_count < max_retries))
+  do
+    output=$(argocd app create $app_name --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --sync-policy none --sync-option CreateNamespace=true --dest-namespace $app_name --dest-server https://kubernetes.default.svc --directory-recurse --upsert -l testing=argoperf 2>&1)
+
+    if [ $? -eq 0 ]; then
+      echo "$output"
+      break
+    else
+      retry_count=$((retry_count + 1))
+      echo "Failed to execute argocd command. Attempt: $retry_count"
+      echo "$output"
+      sleep 5
+    fi
+
+    # If we've reached the max number of retries, exit with failure.
+    if ((retry_count == max_retries)); then
+      echo "Failed to execute argocd command after $max_retries attempts. Stopping execution."
+      exit 1
+    fi
+  done
 
   # Check if sleep interval needs to be applied based on batch interval
   if [ $((i % batch_interval)) -eq 0 ]; then
